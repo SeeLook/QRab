4cbf64335a12297ffba9df3b9e3f30e4b0a25e0c
SeeLook <seelook@gmail.com>
2016-12-18 10:54:13 +0100

    Translations updated, added README

--- lang/qrab_pl.ts
+++ lang/qrab_pl.ts
@@ -4,42 +4,87 @@
 <context>
     <name>AboutDialog</name>
     <message>
-        <location filename="../src/qml/AboutDialog.qml" line="31"/>
         <source>About QRab</source>
-        <translation type="unfinished"></translation>
+        <translation type="vanished">O aplikacji QRab</translation>
     </message>
+</context>
+<context>
+    <name>AboutPage</name>
     <message>
-        <location filename="../src/qml/AboutDialog.qml" line="42"/>
+        <location filename="../src/qml/AboutPage.qml" line="57"/>
         <source>Grabs QR code contexts from your screen</source>
-        <translation type="unfinished"></translation>
+        <translation>Przechwyca zawartość kodu QR z twojego ekranu</translation>
     </message>
     <message>
-        <location filename="../src/qml/AboutDialog.qml" line="44"/>
+        <location filename="../src/qml/AboutPage.qml" line="61"/>
         <source>Author:</source>
+        <translation>Autor:</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/AboutPage.qml" line="69"/>
+        <source>On the terms of GPLv3 license.</source>
+        <translation>Na warunkach licencji GPL wersja 3 </translation>
+    </message>
+</context>
+<context>
+    <name>GrabPage</name>
+    <message>
+        <location filename="../src/qml/GrabPage.qml" line="43"/>
+        <source>Settings</source>
+        <translation>Ustawienia</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/GrabPage.qml" line="51"/>
+        <source>About</source>
+        <translation>O...</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/GrabPage.qml" line="58"/>
+        <source>Put any Qr code on a screen</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
     <name>Main</name>
     <message>
-        <location filename="../src/qml/Main.qml" line="46"/>
         <source>Settings</source>
-        <translation type="unfinished"></translation>
+        <translation type="vanished">Ustawienia</translation>
     </message>
     <message>
-        <location filename="../src/qml/Main.qml" line="52"/>
         <source>About</source>
+        <translation type="vanished">O...</translation>
+    </message>
+</context>
+<context>
+    <name>QPlatformTheme</name>
+    <message>
+        <location filename="../src/qml/AboutPage.qml" line="78"/>
+        <location filename="../src/qml/SettingsPage.qml" line="63"/>
+        <source>OK</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/qml/Main.qml" line="64"/>
-        <source>Put any Qr code on a screen</source>
+        <location filename="../src/qml/SettingsPage.qml" line="58"/>
+        <source>Cancel</source>
         <translation type="unfinished"></translation>
     </message>
+</context>
+<context>
+    <name>SettingsPage</name>
     <message>
-        <location filename="../src/qml/Main.qml" line="83"/>
-        <source>Grab</source>
-        <translation type="unfinished"></translation>
+        <location filename="../src/qml/SettingsPage.qml" line="37"/>
+        <source>Copy QR text to clipboard</source>
+        <translation>Kopiuj text z QR do schowka</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/SettingsPage.qml" line="43"/>
+        <source>Hide window during scanning</source>
+        <translation>Chowaj okno podczas skanowania</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/SettingsPage.qml" line="49"/>
+        <source>Keep QRab window on top</source>
+        <translation>Pozostaw okno QRab na wierzchu</translation>
     </message>
 </context>
 </TS>
