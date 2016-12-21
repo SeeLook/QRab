3c24e4f3cf40e067db9ef9ba999eb526d1f5336c
SeeLook <seelook@gmail.com>
2016-12-21 13:28:26 +0100

    Added license text to new page of About. Common item (NavItem) for switching pages, added icons, updated translations.

--- lang/qrab_pl.ts
+++ lang/qrab_pl.ts
@@ -4,41 +4,129 @@
 <context>
     <name>AboutPage</name>
     <message>
-        <location filename="../src/qml/AboutPage.qml" line="47"/>
+        <location filename="../src/qml/AboutPage.qml" line="61"/>
+        <source>License</source>
+        <translation>Licencja</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/AboutPage.qml" line="87"/>
         <source>Grabs QR code contexts from your screen</source>
         <translation>Przechwyca zawartość kodu QR z twojego ekranu</translation>
     </message>
     <message>
-        <location filename="../src/qml/AboutPage.qml" line="51"/>
+        <location filename="../src/qml/AboutPage.qml" line="92"/>
         <source>Author:</source>
         <translation>Autor:</translation>
     </message>
     <message>
-        <location filename="../src/qml/AboutPage.qml" line="59"/>
+        <location filename="../src/qml/AboutPage.qml" line="99"/>
+        <source>On the terms of GNU GPLv3 license.</source>
+        <translation>Na warunkach licencji GNU GPLv3.</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/AboutPage.qml" line="103"/>
+        <source>QRab uses %1 to handle QR codes.&lt;br&gt;Zbar is delivering on GNU LGPLv2.1 license.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qml/AboutPage.qml" line="127"/>
+        <source>QRab
+Copyright (C) 2016  Tomasz Bojczuk
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software Foundation,
+Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA</source>
+        <translation>QRab
+Copyright (C) 2016  Tomasz Bojczuk
+Ten program jest wolnym oprogramowaniem; możesz go rozprowadzać dalej
+i/lub modyfikować na warunkach Powszechnej Licencji Publicznej GNU,
+wydanej przez Fundację Wolnego Oprogramowania - według wersji 3 tej Licencji
+lub (według twojego wyboru) którejś z późniejszych wersji.
+
+Ten program rozpowszechniany jest z nadzieją, iż będzie on użyteczny,
+jednak BEZ JAKIEJKOLWIEK GWARANCJI, nawet domyślnej gwarancji
+PRZYDATNOŚCI HANDLOWEJ albo PRZYDATNOŚCI DO OKREŚLONYCH ZASTOSOWAŃ.
+W celu uzyskania bliższych informacji sięgnij do Powszechnej Licencji Publicznej GNU.
+
+Z pewnością wraz z niniejszym programem otrzymałeś też egzemplarz
+Powszechnej Licencji Publicznej GNU (GNU General Public License);
+jeśli nie - napisz do Free Software Foundation, Inc., 59 Temple
+Place, Fifth Floor, Boston, MA  02110-1301  USA</translation>
+    </message>
+    <message>
+        <source>QRab
+Copyright (C) 2016  Tomasz Bojczuk
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software Foundation,
+Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA</source>
+        <translation type="vanished">QRab
+Copyright (C) 2016  Tomasz Bojczuk
+Ten program jest wolnym oprogramowaniem; możesz go rozprowadzać dalej
+i/lub modyfikować na warunkach Powszechnej Licencji Publicznej GNU,
+wydanej przez Fundację Wolnego Oprogramowania - według wersji 3 tej Licencji
+lub (według twojego wyboru) którejś z późniejszych wersji.
+Ten program rozpowszechniany jest z nadzieją, iż będzie on użyteczny,
+jednak BEZ JAKIEJKOLWIEK GWARANCJI, nawet domyślnej gwarancji
+PRZYDATNOŚCI HANDLOWEJ albo PRZYDATNOŚCI DO OKREŚLONYCH ZASTOSOWAŃ.
+W celu uzyskania bliższych informacji sięgnij do Powszechnej Licencji Publicznej GNU.
+Z pewnością wraz z niniejszym programem otrzymałeś też egzemplarz
+Powszechnej Licencji Publicznej GNU (GNU General Public License);
+jeśli nie - napisz do Free Software Foundation, Inc., 59 Temple
+Place, Fifth Floor, Boston, MA  02110-1301  USA</translation>
+    </message>
+    <message>
         <source>On the terms of GPLv3 license.</source>
-        <translation>Na warunkach licencji GPL wersja 3. </translation>
+        <translation type="vanished">Na warunkach licencji GPL wersja 3. </translation>
     </message>
 </context>
 <context>
     <name>GrabPage</name>
     <message>
-        <location filename="../src/qml/GrabPage.qml" line="43"/>
+        <location filename="../src/qml/GrabPage.qml" line="53"/>
         <source>Settings</source>
         <translation>Ustawienia</translation>
     </message>
     <message>
-        <location filename="../src/qml/GrabPage.qml" line="51"/>
-        <source>About</source>
-        <translation>O...</translation>
-    </message>
-    <message>
-        <location filename="../src/qml/GrabPage.qml" line="58"/>
-        <source>Put any Qr code on a screen
+        <location filename="../src/qml/GrabPage.qml" line="87"/>
+        <source>Put any QR code on a screen
 Then hit GRAB! button</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../src/qml/GrabPage.qml" line="70"/>
+        <location filename="../src/qml/GrabPage.qml" line="100"/>
+        <source>No QR code found!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qml/GrabPage.qml" line="114"/>
+        <source>Adjust sheet</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/qml/AboutPage.qml" line="55"/>
+        <location filename="../src/qml/GrabPage.qml" line="61"/>
+        <source>About</source>
+        <translation>O...</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/GrabPage.qml" line="120"/>
         <source>GRAB!</source>
         <translation>ŁAP!</translation>
     </message>
@@ -46,13 +134,13 @@ Then hit GRAB! button</source>
 <context>
     <name>Qt</name>
     <message>
-        <location filename="../src/qml/AboutPage.qml" line="68"/>
-        <location filename="../src/qml/SettingsPage.qml" line="63"/>
+        <location filename="../src/qml/AboutPage.qml" line="117"/>
+        <location filename="../src/qml/SettingsPage.qml" line="77"/>
         <source>OK</source>
         <translation>OK</translation>
     </message>
     <message>
-        <location filename="../src/qml/SettingsPage.qml" line="58"/>
+        <location filename="../src/qml/SettingsPage.qml" line="72"/>
         <source>Cancel</source>
         <translation>Anuluj</translation>
     </message>
@@ -60,27 +148,23 @@ Then hit GRAB! button</source>
 <context>
     <name>SettingsPage</name>
     <message>
-        <location filename="../src/qml/SettingsPage.qml" line="37"/>
+        <location filename="../src/qml/SettingsPage.qml" line="44"/>
         <source>Copy QR text to clipboard</source>
         <translation>Kopiuj text z QR do schowka</translation>
     </message>
     <message>
-        <location filename="../src/qml/SettingsPage.qml" line="43"/>
-        <source>Hide window during scanning</source>
-        <translation>Chowaj okno podczas skanowania</translation>
+        <location filename="../src/qml/SettingsPage.qml" line="57"/>
+        <source>Grab delay</source>
+        <translation>Opóżnienie</translation>
     </message>
     <message>
-        <location filename="../src/qml/SettingsPage.qml" line="49"/>
+        <source>Hide window during scanning</source>
+        <translation type="vanished">Chowaj okno podczas skanowania</translation>
+    </message>
+    <message>
+        <location filename="../src/qml/SettingsPage.qml" line="51"/>
         <source>Keep QRab window on top</source>
         <translation>Pozostaw okno QRab na wierzchu</translation>
-    </message>
-</context>
-<context>
-    <name>TgrabQR</name>
-    <message>
-        <location filename="../src/tgrabqr.cpp" line="54"/>
-        <source>No QR code found!</source>
-        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
--- src/qml/AboutPage.qml
+++ src/qml/AboutPage.qml
@@ -16,7 +16,6 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.  *
  ***************************************************************************/
 
-
 import QtQuick 2.7
 import QtQuick.Controls 2.0
 import QtQuick.Layouts 1.3
@@ -34,46 +33,129 @@ Page {
     anchors.margins: defaultSpacing
 
     RowLayout {
-      anchors.horizontalCenter: parent.horizontalCenter
+      Flickable {
+        id: pageSwitch
+        Layout.fillHeight: true
+        width: font.pixelSize * 6
+        clip: true
+        flickableDirection: Flickable.VerticalFlick
 
-      Image {
-        source: "qrc:/icons/qrab-image.png"
-        sourceSize.height: font.pixelSize * 7
+        Rectangle {
+          anchors.fill: parent
+//           border.width: 1
+        }
+
+        ColumnLayout {
+          anchors.fill: parent
+
+          Item { height: font.pixelSize / 2 }
+          NavItem {
+            id: aboutId
+            iconFile: "qrc:/icons/about.png"
+            text: qsTranslate("GrabPage", "About")
+            onClicked: stackLay.currentIndex = 0
+          }
+          NavItem {
+            id: licenseId
+            iconFile: "qrc:/icons/license.png"
+            text: qsTr("License")
+            onClicked: stackLay.currentIndex = 1
+          }
+          Item { Layout.fillHeight: true }
+        }
       }
-      Text {
-        text: "  QRab 0.1"
-        font.bold: true
-        Component.onCompleted: font.pixelSize = font.pixelSize * 2
+
+      StackLayout {
+        id: stackLay
+        ColumnLayout { // FIRST page with info about QRab
+          Layout.fillWidth: true
+          anchors.horizontalCenter: parent.horizontalCenter
+
+          RowLayout {
+            anchors.horizontalCenter: parent.horizontalCenter
+
+            Image {
+              source: "qrc:/icons/qrab-image.png"
+              sourceSize.height: font.pixelSize * 7
+            }
+            Text {
+              text: "  QRab 0.1"
+              font.bold: true
+              Component.onCompleted: font.pixelSize = font.pixelSize * 2
+            }
+          }
+          Text {
+            text: qsTr("Grabs QR code contexts from your screen")
+            anchors.horizontalCenter: parent.horizontalCenter
+          }
+          RowLayout {
+            anchors.horizontalCenter: parent.horizontalCenter
+            Text { text: qsTr("Author:") }
+            Text {
+              text: " Tomasz Bojczuk"
+              font.bold: true
+            }
+          }
+          Text {
+            text: qsTr("On the terms of GNU GPLv3 license.") 
+            anchors.horizontalCenter: parent.horizontalCenter
+          }
+          Text {
+            text: qsTr("QRab uses %1 to handle QR codes.<br>Zbar is delivering on GNU LGPLv2.1 license.").arg("<b><a href=\"http://zbar.sf.net\">zbar</a></b>")
+            anchors.horizontalCenter: parent.horizontalCenter
+            horizontalAlignment: Text.AlignHCenter
+            onLinkActivated: Qt.openUrlExternally(link)
+            MouseArea { // make hand cursor over link text
+              anchors.fill: parent
+              acceptedButtons: Qt.NoButton
+              cursorShape: parent.hoveredLink ? Qt.PointingHandCursor : Qt.ArrowCursor
+            }
+          }
+          Item { Layout.fillHeight: true } // a spacer
+        }
+        Flickable {
+          Layout.fillHeight: true
+          clip: true
+          flickableDirection: Flickable.VerticalFlick
+          contentWidth: secondPage.width
+          contentHeight: secondPage.height
+          ColumnLayout { // SECOND page with license text
+            id: secondPage
+            Label {
+              text: qsTr("QRab
+Copyright (C) 2016  Tomasz Bojczuk
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software Foundation,
+Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA"
+                    )
+              horizontalAlignment: Text.AlignJustify
+            }
+            Text {
+              text: "<a href=\"https://www.gnu.org/licenses/gpl-3.0.html\">https://www.gnu.org/licenses/gpl-3.0.html</a>"
+              anchors.horizontalCenter: parent.horizontalCenter
+              font.bold: true
+              onLinkActivated: Qt.openUrlExternally(link)
+              MouseArea { // make hand cursor over link text
+                anchors.fill: parent
+                acceptedButtons: Qt.NoButton
+                cursorShape: parent.hoveredLink ? Qt.PointingHandCursor : Qt.ArrowCursor
+              }
+            }
+            Item { height: aboutPage.font.pixelSize * 3 }
+          }
+        }
       }
     }
-    Text {
-      text: qsTr("Grabs QR code contexts from your screen")
-      anchors.horizontalCenter: parent.horizontalCenter
-    }
-    RowLayout {
-      anchors.horizontalCenter: parent.horizontalCenter
-      Text { text: qsTr("Author:") }
-      Text {
-        text: " Tomasz Bojczuk"
-        font.bold: true
-      }
-    }
-    Text {
-      text: qsTr("On the terms of GNU GPLv3 license.") 
-      anchors.horizontalCenter: parent.horizontalCenter
-    }
-    Text {
-      text: qsTr("QRab uses %1 to handle QR codes.<br>Zbar is delivering on GNU LGPLv2.1 license.").arg("<b><a href=\"http://zbar.sf.net\">zbar</a></b>")
-      anchors.horizontalCenter: parent.horizontalCenter
-      horizontalAlignment: Text.AlignHCenter
-      onLinkActivated: Qt.openUrlExternally(link)
-      MouseArea { // make hand cursor over link text
-        anchors.fill: parent
-        acceptedButtons: Qt.NoButton
-        cursorShape: parent.hoveredLink ? Qt.PointingHandCursor : Qt.ArrowCursor
-      }
-    }
-    Item { Layout.fillHeight: true } // a spacer
     RowLayout {
       Item { Layout.fillWidth: true }
       Button {
